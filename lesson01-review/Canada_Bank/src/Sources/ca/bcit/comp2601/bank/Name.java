a7feffcf94300c89f47bcf895843b0893cc0429b
TrevisanGMW <trevisangmw@gmail.com>
2022-09-10 16:05:19 -0700

    Update name validation

--- lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/Name.java
+++ lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/Name.java
@@ -4,35 +4,21 @@ public class Name
 {
     private final String first;
     private final String last;
+    private static final int NAME_MAX_LENGTH = 45;
+    private static final String NAME_INVALID_STRING = "admin";
 
     /**
      * Create a public constructor that takes first, last as arguments:
      * they must not be null nor blank;
      * must be fewer than 45 characters;
      * must not contain the word "admin" (in any letter casing)
-     * @param first
-     * @param last
+     * @param first first name string
+     * @param last last name string
      */
     Name(String first, String last)
     {
-        if(first != null && first != "" && first.toLowerCase() != "admin" && first.length() < 46)
-        {
-            this.first = first;
-        }
-        else
-        {
-            throw new IllegalArgumentException("Invalid first name");
-        }
-
-        if(last != null && last != ""&& first.toLowerCase() != "admin" && first.length() < 46)
-        {
-            this.last = last;
-        }
-        else
-        {
-            throw new IllegalArgumentException("Invalid last name");
-        }
-
+        this.first = validateName(first);
+        this.last = validateName(last);
     }
 
     public String getFirst()
@@ -55,6 +41,29 @@ public class Name
         return first + " " + last;
     }
 
+    private static String validateName(String inputString)
+    {
+        if(inputString == null)
+        {
+            throw new IllegalArgumentException("Invalid name. It cannot be null.");
+        }
+        else if (inputString.replaceAll("\\s+","").equals(""))
+        {
+            throw new IllegalArgumentException("Invalid name. Cannot be empty.");
+        }
+        else if (inputString.toLowerCase().contains(NAME_INVALID_STRING))
+        {
+            throw new IllegalArgumentException("Invalid name. Cannot be \"" + NAME_INVALID_STRING + "\".");
+        }
+        else if ( inputString.length() <= NAME_MAX_LENGTH)
+        {
+            throw new IllegalArgumentException("Invalid name. Cannot have more than 45.");
+        }
+        else
+        {
+            return inputString;
+        }
+    }
 
 
 }
