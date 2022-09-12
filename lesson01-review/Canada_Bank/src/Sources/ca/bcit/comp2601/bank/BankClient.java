6bf1b054f6539647f77a086212d3b601a19492ed
TrevisanGMW <trevisangmw@gmail.com>
2022-09-11 19:33:48 -0700

    Created BankClient class and added requirements

---
+++ lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/BankClient.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2601.bank;
+
+/**
+ * BankClient (extends Person; has a dateJoinedBank Date; has a String clientID which is 5 digits;
+ * overrides getDetails() to return a String in the exact format of
+ * "Tiger Woods client #12345 (alive) joined the bank on thursday, September 3, 2020")
+ * [note: or “not alive” as the case may be].
+ *
+ * Instance variables, constructor arguments, accessor methods for:
+ *  client ID: 5 digits
+ *  dateJoinedTheBank
+ *
+ * Methods:
+ * getDetails()
+ *
+ */
+
+public class BankClient
+{
+}
