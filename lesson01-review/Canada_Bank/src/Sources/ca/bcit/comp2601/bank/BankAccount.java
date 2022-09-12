48bef95261b5bf1f114008c1a3b34d8456f2083a
TrevisanGMW <trevisangmw@gmail.com>
2022-09-11 19:33:32 -0700

    Created BankAccount class and added requirements

---
+++ lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/BankAccount.java
@@ -0,0 +1,21 @@
+package ca.bcit.comp2601.bank;
+
+/**
+ * BankAccount (has overloaded methods for withdraw: withdraw(final double amountUsd);
+ * withdraw(final double amountUsd, final int pinToMatch); has a client;
+ * has an account number which is 7 letters; has a Date for accountOpened;
+ * has a Date for accountClosed which can be null if it is not closed)
+ *
+ * Instance variables, constructor arguments, accessor methods for:
+ * client balanceUsd pin
+ * account number: 7 letters
+ *
+ * Methods
+ * deposit()
+ * withdraw(amountUsd)
+ * withdraw(amountUsd, pinToMatch)
+ *
+ */
+public class BankAccount
+{
+}
