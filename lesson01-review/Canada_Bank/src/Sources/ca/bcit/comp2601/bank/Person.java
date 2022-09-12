99435af1e00c93f6c0a39463b7610d1817f8fe1b
TrevisanGMW <trevisangmw@gmail.com>
2022-09-11 19:34:16 -0700

    Added more description to Person class

--- lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/Person.java
+++ lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/Person.java
@@ -1,10 +1,31 @@
 package ca.bcit.comp2601.bank;
 
+
+/**
+ * Person (has a Name and a birthDate and a deathDate [which can be null];
+ * has a getDetails() method which returns a String in the exact format of
+ * "Tiger Woods (alive) was born on tuesday, December 30, 1975!" or
+ * “Albert Einstein (died monday, April 18, 1955) was born on friday, March 14, 1879!”.
+ *
+ * Instance variables, constructor arguments, accessor methods for:
+ * name
+ * date born
+ * date died
+ *
+ * Methods:
+ * getDetails()
+ * isAlive()
+ *
+ */
 public class Person
 {
     private final Name name;
+    private final Name birthDate;
+    private final Name deathDate; // can be null
 
-    public Person(Name name) {
+    public Person(Name name, Name birthDate, Name deathDate) {
         this.name = name;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
     }
 }
