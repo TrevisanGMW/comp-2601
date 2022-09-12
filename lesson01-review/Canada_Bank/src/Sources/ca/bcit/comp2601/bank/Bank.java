41990effb65f3c2b2bc9e90efa2047d61496bc15
TrevisanGMW <trevisangmw@gmail.com>
2022-09-11 19:33:16 -0700

    Created Bank class and added requirements

---
+++ lesson01-review/Canada_Bank/src/Sources/ca/bcit/comp2601/bank/Bank.java
@@ -0,0 +1,23 @@
+package ca.bcit.comp2601.bank;
+
+/**
+ * Bank (has a CEO who is a Person; has an ArrayList of BankAccount references;
+ * addAccount() throws an IllegalArgumentException if an account is being added but its accountNumber is
+ * already in the Bank's account collection; getCeo() returns the current CEO)
+ *
+ *
+ * Instance variables, constructor arguments, accessor methods for:
+ * ArrayList of BankAccounts
+ *
+ * Methods:
+ * addAccount(newAccount)
+ * removeAccount(accountNumber)
+ * getAllAccounts() // an Array
+ * getMaxAccount() // most $
+ * getAccountFor(client ID)
+ *
+ */
+
+public class Bank
+{
+}
