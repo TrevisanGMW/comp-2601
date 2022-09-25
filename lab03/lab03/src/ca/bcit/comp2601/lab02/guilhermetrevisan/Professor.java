747ad0ca9dc51fcfc73597c3abfacbcff30c457c
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-24 22:35:15 -0700

    Overridden equals, hashCode, and compareTo

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Professor.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Professor.java
@@ -1,5 +1,7 @@
 package ca.bcit.comp2601.lab02.guilhermetrevisan;
 
+import java.util.Objects;
+
 /**
  * Professor (“einstein”, “fancy”, true, true, “teach”);
  * OVERTIME_PAY_RATE: 2.0
@@ -8,13 +10,14 @@ package ca.bcit.comp2601.lab02.guilhermetrevisan;
  * @version 0.0.1
  * @since 2022-09-24
  */
-public class Professor extends Employee{
+public class Professor extends Employee implements Comparable<Professor> {
 
     private static final double OVERTIME_PAY_RATE = 2.0d;
     private static final boolean IS_PAID_SALARY = true;
     private static final boolean IS_POST_SECONDARY_EDUCATION_REQUIRED = true;
     private static final String WORK_VERB = "teach";
     private static final String DRESS_CODE = "fancy";
+    private static final String MOST_PRESTIGIOUS_MAJOR = "Computer Science";
     private String teachingMajor;
 
     /**
@@ -54,5 +57,49 @@ public class Professor extends Employee{
     @Override
     public double getOverTimePayRate() {
         return OVERTIME_PAY_RATE;
+    }
+
+    /**
+     * Override equals
+     * @param o object to compare
+     * @return true if teachingMajor is the same
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return teachingMajor.equals(professor.teachingMajor);
+    }
+
+    /**
+     * Override hashCode
+     * @return hashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(teachingMajor);
+    }
+
+    /**
+     * Override compareTo
+     * @param that the object to be compared.
+     * @return comparison int, positive if major is MOST_PRESTIGIOUS_MAJOR, else it's equal
+     */
+    @Override
+    public int compareTo(Professor that) {
+        if (this.getTeachingMajor() == null && that.getTeachingMajor() == null) {
+            return 0;
+        } else if (this.getTeachingMajor() == null) {
+            return -1;
+        } else if (that.getTeachingMajor() == null) {
+            return 1;
+        } else {
+            if (this.getTeachingMajor().equalsIgnoreCase(MOST_PRESTIGIOUS_MAJOR)){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
