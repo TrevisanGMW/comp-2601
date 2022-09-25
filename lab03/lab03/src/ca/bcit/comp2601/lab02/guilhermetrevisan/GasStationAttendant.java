519859e770ae5c17beb6944ef50543f960a6c907
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-24 22:34:54 -0700

    Overridden equals, hashCode, and compareTo

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/GasStationAttendant.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/GasStationAttendant.java
@@ -1,5 +1,7 @@
 package ca.bcit.comp2601.lab02.guilhermetrevisan;
 
+import java.util.Objects;
+
 /**
  * Example:
  * GasStationAttendant(“smith”, “uniform”, false, false, “pump”);
@@ -9,7 +11,7 @@ package ca.bcit.comp2601.lab02.guilhermetrevisan;
  * @version 0.0.1
  * @since 2022-09-24
  */
-public class GasStationAttendant extends Employee{
+public class GasStationAttendant extends Employee implements Comparable<GasStationAttendant> {
 
     private static final double OVERTIME_PAY_RATE = 1.5d;
     private static final boolean IS_PAID_SALARY = false;
@@ -54,5 +56,40 @@ public class GasStationAttendant extends Employee{
     @Override
     public double getOverTimePayRate() {
         return OVERTIME_PAY_RATE;
+    }
+
+    /**
+     * Override equals
+     * @param o object to compare
+     * @return true if same numberOfDollarsStolenPerDayUSD
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GasStationAttendant that = (GasStationAttendant) o;
+        return Double.compare(that.numberOfDollarsStolenPerDayUSD, numberOfDollarsStolenPerDayUSD) == 0;
+    }
+
+    /**
+     * Override hashCode
+     * @return hashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberOfDollarsStolenPerDayUSD);
+    }
+
+    /**
+     * Override compareTo
+     * @param that the object to be compared.
+     * @return comparison int, positive if getNumberOfDollarsStolenPerDayUSD is higher
+     */
+    @Override
+    public int compareTo(GasStationAttendant that) {
+        if (this.getNumberOfDollarsStolenPerDayUSD() != that.getNumberOfDollarsStolenPerDayUSD()) {
+            return (this.getNumberOfDollarsStolenPerDayUSD() < that.getNumberOfDollarsStolenPerDayUSD() ? -1 : 1);
+        }
+        return 0;
     }
 }
