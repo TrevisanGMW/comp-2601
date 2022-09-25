053bbdaa5660416b7b3e434496353c0afff0343c
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-24 22:35:09 -0700

    Overridden equals, hashCode, and compareTo

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Parent.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Parent.java
@@ -1,5 +1,7 @@
 package ca.bcit.comp2601.lab02.guilhermetrevisan;
 
+import java.util.Objects;
+
 /**
  * Parent(“mother”, “anything”, false, false, “care”);
  * OVERTIME_PAY_RATE: -2.0
@@ -8,7 +10,7 @@ package ca.bcit.comp2601.lab02.guilhermetrevisan;
  * @version 0.0.1
  * @since 2022-09-24
  */
-public class Parent extends Employee{
+public class Parent extends Employee implements Comparable<Parent> {
 
     private static final double OVERTIME_PAY_RATE = -2.0d;
     private static final int MIN_NUM_HOURS_SPENT_PER_WEEK = 0;
@@ -73,5 +75,40 @@ public class Parent extends Employee{
     @Override
     public boolean getsPaid() {
         return GETS_PAID;
+    }
+
+    /**
+     * Overide equals
+     * @param o object to compare to
+     * @return true if same number of hours spent per week with kids
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Parent parent = (Parent) o;
+        return numberOfHoursSpentPerWeekWithKids == parent.numberOfHoursSpentPerWeekWithKids;
+    }
+
+    /**
+     * Override hashCode
+     * @return hashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberOfHoursSpentPerWeekWithKids);
+    }
+
+    /**
+     * Override compareTo
+     * @param that the object to be compared.
+     * @return comparison int, higher if getNumberOfHoursSpentPerWeekWithKids is bigger
+     */
+    @Override
+    public int compareTo(Parent that) {
+        if (this.getNumberOfHoursSpentPerWeekWithKids() != that.getNumberOfHoursSpentPerWeekWithKids()) {
+            return (this.getNumberOfHoursSpentPerWeekWithKids() < that.getNumberOfHoursSpentPerWeekWithKids() ? -1 : 1);
+        }
+        return 0;
     }
 }
