eb39f5be9d67b1bc37f87b2a9c053bec6ba0adb0
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-25 16:39:56 -0700

    Added sort and print functions

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Employees.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Employees.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2601.lab02.guilhermetrevisan;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,35 +15,115 @@ import java.util.List;
 public class Employees {
 
     private final List<Employable> employeesList;
+    private final List<HockeyPlayer> hockeyPlayersList;
+    private final List<Professor> professorsList;
+    private final List<Parent> parentsList;
+    private final List<GasStationAttendant> gasStationAttendantsList;
 
     public Employees() {
-        employeesList = new ArrayList<>();
+        employeesList = new ArrayList<>(); // All of them combined
+        hockeyPlayersList = new ArrayList<>();
+        professorsList = new ArrayList<>();
+        parentsList = new ArrayList<>();
+        gasStationAttendantsList = new ArrayList<>();
 
-        employeesList.add(new HockeyPlayer("Wayne Gretzky", 894));
-        employeesList.add(new HockeyPlayer("Who Ever", 0));
-        employeesList.add(new HockeyPlayer("Brent Gretzky", 1));
-        employeesList.add(new HockeyPlayer("Pavel Bure", 437));
-        employeesList.add(new HockeyPlayer("Jason Harrison", 0));
+        hockeyPlayersList.add(new HockeyPlayer("Wayne Gretzky", 894));
+        hockeyPlayersList.add(new HockeyPlayer("Who Ever", 0));
+        hockeyPlayersList.add(new HockeyPlayer("Brent Gretzky", 1));
+        hockeyPlayersList.add(new HockeyPlayer("Pavel Bure", 437));
+        hockeyPlayersList.add(new HockeyPlayer("Jason Harrison", 0));
 
-        employeesList.add(new Professor("Albert Einstein", "Physics"));
-        employeesList.add(new Professor("Jason Harrison", "Computer Systems"));
-        employeesList.add(new Professor("Richard Feynman", "Physics"));
-        employeesList.add(new Professor("BCIT Instructor", "Computer Systems"));
-        employeesList.add(new Professor("Kurt Godel", "Logic"));
+        professorsList.add(new Professor("Albert Einstein", "Physics"));
+        professorsList.add(new Professor("Jason Harrison", "Computer Systems"));
+        professorsList.add(new Professor("Richard Feynman", "Physics"));
+        professorsList.add(new Professor("BCIT Instructor", "Computer Systems"));
+        professorsList.add(new Professor("Kurt Godel", "Logic"));
 
-        employeesList.add(new Parent("Tiger Woods", 1));
-        employeesList.add(new Parent("Super Mom", 168));
-        employeesList.add(new Parent("Lazy Larry", 20));
-        employeesList.add(new Parent("Ex Hausted", 168));
-        employeesList.add(new Parent("Super Dad", 167));
+        parentsList.add(new Parent("Tiger Woods", 1));
+        parentsList.add(new Parent("Super Mom", 168));
+        parentsList.add(new Parent("Lazy Larry", 20));
+        parentsList.add(new Parent("Ex Hausted", 168));
+        parentsList.add(new Parent("Super Dad", 167));
 
-        employeesList.add(new GasStationAttendant("Joe Smith", 10));
-        employeesList.add(new  GasStationAttendant("Tony Baloney", 100));
-        employeesList.add(new  GasStationAttendant("Benjamin Franklin", 100));
-        employeesList.add(new GasStationAttendant("Mary Fairy", 101));
-        employeesList.add(new GasStationAttendant("Bee See", 1));
+        gasStationAttendantsList.add(new GasStationAttendant("Joe Smith", 10));
+        gasStationAttendantsList.add(new GasStationAttendant("Tony Baloney", 100));
+        gasStationAttendantsList.add(new GasStationAttendant("Benjamin Franklin", 100));
+        gasStationAttendantsList.add(new GasStationAttendant("Mary Fairy", 101));
+        gasStationAttendantsList.add(new GasStationAttendant("Bee See", 1));
+
+        // Populate employeesList
+        employeesList.addAll(hockeyPlayersList);
+        employeesList.addAll(professorsList);
+        employeesList.addAll(parentsList);
+        employeesList.addAll(gasStationAttendantsList);
+
     }
 
+    /**
+     * @param employeesListToPrint employeesList the employable list to print
+     */
+    private void printEmployeeNames(final List employeesListToPrint){
+        ArrayList<Employable> employees;
+        employees = (ArrayList<Employable>)employeesListToPrint;
+        for (Employable employee : employees)
+        {
+            Employee e = (Employee)employee;
+            System.out.println(e.getName());
+        }
+    }
+
+    /**
+     * sortEmployeeList sorts input list using compareTo
+     */
+    public void sortAllEmployeeLists() {
+        Collections.sort(hockeyPlayersList);
+        Collections.sort(professorsList);
+        Collections.sort(parentsList);
+        Collections.sort(gasStationAttendantsList);
+    }
+
+    /**
+     * Prints the name of all employees found in the employeesList
+     */
+    public void printAllEmployees(){
+        printEmployeeNames(employeesList);
+    }
+
+    /**
+     * Prints the name of all employees found in the employeesList
+     */
+    public void printAllHockeyPlayers(){
+        printEmployeeNames(hockeyPlayersList);
+    }
+
+    /**
+     * Prints the name of all employees found in the employeesList
+     */
+    public void printAllParents(){
+        printEmployeeNames(parentsList);
+    }
+
+    /**
+     * Prints the name of all employees found in the employeesList
+     */
+    public void printAllGasStationAttendants(){
+        printEmployeeNames(gasStationAttendantsList);
+    }
+
+
+//    /**
+//     * Prints/Displays all employees that are equal
+//     * @param a
+//     */
+//    static public void getMax(final int[] a)    {
+//        int max = a[0];
+//        for(int i: a)        {
+//            if(i > max)            {
+//                max = i;
+//            }
+//        }
+//
+//    }
 
 
 }
