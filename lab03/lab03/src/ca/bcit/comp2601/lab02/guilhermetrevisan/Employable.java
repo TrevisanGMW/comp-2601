de69f8a2e877282023e6b4873c6255850f473c5e
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-26 21:01:45 -0700

    Added javadocs

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Employable.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Employable.java
@@ -1,17 +1,42 @@
 package ca.bcit.comp2601.lab02.guilhermetrevisan;
 
 /**
+ * Employable interface
  * @author Guilherme Trevisan
  * @version 0.0.1
  * @since 2022-09-24
  */
 public interface Employable {
 
-    public String getDressCode();
-    public boolean isPaidSalary();
-    public boolean postSecondaryEducationRequired();
-    public String getWorkVerb();
-    default public boolean getsPaid(){
+    /**
+     * Getter Dress Code
+     * @return string representing the dress code
+     */
+    String getDressCode();
+
+    /**
+     * Getter is paid salary
+     * @return if the employable object is paid salary or not
+     */
+    boolean isPaidSalary();
+
+    /**
+     * Getter post secondary requirement
+     * @return if the employable object needs post secondary education or not
+     */
+    boolean postSecondaryEducationRequired();
+
+    /**
+     * Getter work verb
+     * @return returns a description of the work verb (purpose)
+     */
+    String getWorkVerb();
+
+    /**
+     * Default gets paid state
+     * @return true (if not overridden)
+     */
+    default boolean getsPaid(){
         return true;
     }
 
