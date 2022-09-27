e9630310d282ef71503eeb8c6d826750fbf4a446
Guilherme Trevisan <trevisangmw@gmail.com>
2022-09-26 21:21:01 -0700

    Added printAllEqualEmployees

--- lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Main.java
+++ lab03/lab03/src/ca/bcit/comp2601/lab02/guilhermetrevisan/Main.java
@@ -5,11 +5,44 @@ public class Main {
         Employees employees;
         employees = new Employees();
 
-        System.out.println("- Before:");
-        employees.printAllParents();
+        final String separator;
+        separator = "___________________________";
 
-        System.out.println("- After:");
-        employees.sortAllEmployeeLists();
+        // Hockey Players
+        System.out.println(" - Hockey Players - Before:");
+        employees.printAllHockeyPlayers();
+        System.out.println(" - Hockey Players - After:");
+        employees.sortHockeyPlayersList();
+        employees.printAllHockeyPlayers();
+        System.out.println(separator);
+
+        // Professors
+        System.out.println(" - Professors - Before:");
+        employees.printAllProfessors();
+        System.out.println(" - Professors - After:");
+        employees.sortProfessorsList();
+        employees.printAllProfessors();
+        System.out.println(separator);
+
+        // Parents
+        System.out.println(" - Parents - Before:");
         employees.printAllParents();
+        System.out.println(" - Parents - After:");
+        employees.sortParentsList();
+        employees.printAllParents();
+        System.out.println(separator);
+
+        // Gas Station Attendants
+        System.out.println(" - Gas Station Attendants - Before:");
+        employees.printAllGasStationAttendants();
+        System.out.println(" - Gas Station Attendants - After:");
+        employees.sortGasStationAttendantsList();
+        employees.printAllGasStationAttendants();
+        System.out.println(separator);
+
+        System.out.println(" - Equal Employees:");
+        employees.printAllEqualEmployees();
+//        Employees.getEqualEmployeesList(employees); // In case it needs to be done purely through a method
+
     }
 }
