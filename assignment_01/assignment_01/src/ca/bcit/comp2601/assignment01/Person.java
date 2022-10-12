77931d0fef74b6d4565547cb15fba109b30e07ce
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-11 19:43:44 -0700

    Imported content from lab01

--- assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Person.java
+++ assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Person.java
@@ -21,5 +21,94 @@ package ca.bcit.comp2601.assignment01;
  * @since 2022-10-10
  */
 
-public class Person {
+public class Person implements Comparable {
+    private final Name name;
+    private final Date born;
+    private final Date died;
+
+    /**
+     * @param name Name object
+     * @param born Date object for the birth date
+     * @param died (can be null)
+     * @throws IllegalArgumentException if any arguments are invalid
+     */
+    public Person(Name name, Date born, Date died)
+    {
+        if (name != null)
+        {
+            this.name = name;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid argument. Name cannot be null.");
+        }
+
+        if (born != null)
+        {
+            this.born = born;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid argument. birthDate cannot be null.");
+        }
+        this.died = died;
+    }
+
+    /**
+     *  Returns customized sentence with details about the person
+     *  "Tiger Woods (alive) was born on tuesday, December 30, 1975!"
+     *  "Albert Einstein (died monday, April 18, 1955) was born on friday, March 14, 1879!”
+     * @return String following the format mentioned above
+     */
+    public String getDetails()
+    {
+        String result;
+        String livingCondition;
+
+        if (isAlive())
+        {
+            livingCondition = "(alive)";
+        }
+        else
+        {
+            livingCondition = "(died " + died.getDayOfTheWeek().toLowerCase() +
+                    ", " + died.getDateAsText() +  ")";
+        }
+
+        result = name.getFullName() + " " + livingCondition + " was born on " +
+                born.getDayOfTheWeek().toLowerCase() + ", " + born.getDateAsText() + "!";
+        return result;
+    }
+
+    /**
+     * @return true if the person is alive (in case deathDate is not null)
+     */
+    public boolean isAlive()
+    {
+        return (this.died == null);
+    }
+
+    /**
+     * Getter name
+     * @return name
+     */
+    public Name getName() {
+        return name;
+    }
+
+    /**
+     * Getter birthDate
+     * @return birthDate
+     */
+    public Date getBorn() {
+        return born;
+    }
+
+    /**
+     * Getter deathDate
+     * @return deathDate
+     */
+    public Date getDied() {
+        return died;
+    }
 }
