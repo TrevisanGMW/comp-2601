d9b9e4a440978ecd483c874e30c18988b391a5ab
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:23:20 -0700

    Created interface with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/FileManager.java
@@ -0,0 +1,9 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+/**
+ * o	void save(final String fileName)
+ * o	void delete(final String fileName);
+ * o	more on this later
+ */
+public interface FileManager {
+}
