37cf4330fb349788ad5c5cdef304cd03b6a5235a
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-16 12:56:46 -0700

    Completed initial requirements

--- lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/CompactDisc.java
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/CompactDisc.java
@@ -1,13 +1,76 @@
 package ca.bcit.comp2601.lab04.guilhermetrevisan;
 
 /**
- * o	Inherits from MusicMedia
- * o	Has a field to hold the number of tracks on the CD
- * o	Has a static field called “READING_METHOD” whose value is “laser”
- * o	Constructor takes parameters for title, artist. The number of tracks. The number of tracks should be validated appropriately and an IllegalArgumentException will be thrown if the value is bad.
- * o	Getter and setter for the number of tracks field
- * o	toString that will display the number of tracks, title, and artist
- * o	play() method will display a simple message indicating that the cd is being played.
+ * CompactDisc Class
+ * @author Guilherme Trevisan
+ * @version 0.0.1
+ * @since   2022-09-16
  */
 public class CompactDisc extends MusicMedia {
+
+    private int numOfTracks;
+    private static final String READING_METHOD = "laser";
+
+    /**
+     * CompactDisc Constructor
+     * @param title title
+     * @param artist artist
+     * @param numOfTracks numOfTracks
+     */
+    public CompactDisc(final String title,
+                       final String artist,
+                       final int numOfTracks) {
+        super(title, artist);
+        this.numOfTracks = validateNumOfTracks(numOfTracks);
+    }
+
+    /**
+     * Validate the number of tracks
+     * @param numOfTracks numOfTracks
+     * @return valid int value
+     * @throws IllegalArgumentException if less than zero
+     */
+    private static int validateNumOfTracks(final int numOfTracks){
+        if(numOfTracks < 0) { // Zero is ok, a disk might be empty
+            throw new IllegalArgumentException("Invalid number of tracks. It cannot be less than zero.");
+        }
+        else {
+            return numOfTracks;
+        }
+    }
+
+    // Getters and Setters
+    /**
+     * Getter numOfTracks
+     * @return numOfTracks
+     */
+    public int getNumOfTracks() {
+        return numOfTracks;
+    }
+
+    /**
+     * Setter numOfTracks
+     * @param numOfTracks numOfTracks
+     */
+    public void setNumOfTracks(int numOfTracks) {
+        this.numOfTracks = validateNumOfTracks(numOfTracks);
+    }
+
+    // Misc and Overrides
+    /**
+     * method void play()
+     */
+    @Override
+    public void play() {
+        System.out.println("Playing: " + this.getTitle() + " by " + this.getArtist());
+    }
+
+    /**
+     * display the number of tracks, title, and artist
+     * @return string following this pattern "Artist - Title (Number of tracks : #)"
+     */
+    @Override
+    public String toString() {
+        return this.getArtist() + " - " + this.getTitle() + " (Number of tracks:" + numOfTracks + ")";
+    }
 }
