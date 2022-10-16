3386600531fec13af56239bb7943841f5fe92c03
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:22:19 -0700

    Created class with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/AudioFile.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+/**
+ * o	Fields for the file name and the file size in bytes per second (eg. 320 bps)
+ * o	Constructor with parameters for title, artist, filename, file size. File name and file size must be validated appropriately and an IllegalArgumentException will be thrown if the values are bad.
+ * o	Getters, setters toString
+ * o	play() method will display a simple message indicating that the file is being played.
+ * o	Implements FileManager -  methods need only output a simple message to the console.
+ */
+public class AudioFile {
+}
