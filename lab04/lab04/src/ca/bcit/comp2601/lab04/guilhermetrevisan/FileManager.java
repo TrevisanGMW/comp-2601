c7d0b4141ffca4cec7b7221de4a0e38d213a3854
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-16 16:45:41 -0700

    Added javadocs

--- lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/FileManager.java
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/FileManager.java
@@ -1,11 +1,21 @@
 package ca.bcit.comp2601.lab04.guilhermetrevisan;
 
 /**
- * o	void save(final String fileName)
- * o	void delete(final String fileName);
- * o	more on this later
+ * FileManager Interface
+ * @author Guilherme Trevisan
+ * @version 0.0.1
+ * @since   2022-09-16
  */
 public interface FileManager {
+    /**
+     * Save the file using the provided file name
+     * @param fileName fileName
+     */
+
     void save(final String fileName);
+    /**
+     * Deletes the file using the provided file name
+     * @param fileName fileName
+     */
     void delete(final String fileName);
 }
