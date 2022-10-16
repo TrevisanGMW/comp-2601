a47c0e6e3ccb9f7eddae240ceabc28291d2d031b
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-16 12:57:04 -0700

    Completed initial requirements

--- lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/Driver.java
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/Driver.java
@@ -2,17 +2,22 @@ package ca.bcit.comp2601.lab04.guilhermetrevisan;
 
 
 /**
- * Expand the Driver class to create one of each of the MusicMedia types, assigning each address to a MusicMedia reference. Display and the objects’ data to the console and call the various methods you have overridden in the subclasses.
- * Make sure to use the following in your code:
- * -	initializer block
- * -	this()
- * -	super()
- * -	static initializer block
- * -	static validation method
- * Submit your project to the dropbox before the deadline.
+ * See docs
  */
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create a CompactDisc object and assign the address to a MusicMedia reference
+        CompactDisc cd1;
+        MusicMedia mm1;
+        cd1 = new CompactDisc("TitleOne", "ArtistOne", 7);
+        mm1 = cd1;
+
+        // display the objects' field values
+        System.out.println(cd1.toString());
+        System.out.println(mm1.toString());
+
+        // call the play() method.
+        cd1.play();
+        mm1.play();
     }
 }
