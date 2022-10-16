a20613fab3bc2a617e831398a9169965254a1d38
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:22:27 -0700

    Created class with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/CompactDisc.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+/**
+ * o	Inherits from MusicMedia
+ * o	Has a field to hold the number of tracks on the CD
+ * o	Has a static field called “READING_METHOD” whose value is “laser”
+ * o	Constructor takes parameters for title, artist. The number of tracks. The number of tracks should be validated appropriately and an IllegalArgumentException will be thrown if the value is bad.
+ * o	Getter and setter for the number of tracks field
+ * o	toString that will display the number of tracks, title, and artist
+ * o	play() method will display a simple message indicating that the cd is being played.
+ */
+public class CompactDisc extends MusicMedia {
+}
