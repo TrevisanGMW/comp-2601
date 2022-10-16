e5784e5bde0cf27952ada17deeee7e1a0299956c
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:22:42 -0700

    Created class with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/Driver.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+
+/**
+ * Expand the Driver class to create one of each of the MusicMedia types, assigning each address to a MusicMedia reference. Display and the objects’ data to the console and call the various methods you have overridden in the subclasses.
+ * Make sure to use the following in your code:
+ * -	initializer block
+ * -	this()
+ * -	super()
+ * -	static initializer block
+ * -	static validation method
+ * Submit your project to the dropbox before the deadline.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
