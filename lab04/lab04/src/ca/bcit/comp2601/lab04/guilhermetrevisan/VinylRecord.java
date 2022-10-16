bd27514faa95b06e07dd119f274281694c7d7ab6
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:23:08 -0700

    Created class with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/VinylRecord.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+/**
+ * o	Fields for the number of tracks, the size, and the weight in grams
+ * o	Constructor will take parameters for title, artist, number of tracks, size, and weight. Appropriate validations will be done and IllegalArgumentExeptions will be thrown when values are bad.
+ * o	Valid size will be either 12”, 10” or 7”
+ * o	Valid weight will be either:
+ * 	140g, 180g, 200g for a 12”
+ * 	100g for a 10”
+ * 	40g for a 7” record
+ * o	Getter, setters toString
+ * o	play() method will display a simple message indicating that the record is being played.
+ */
+public class VinylRecord {
+}
