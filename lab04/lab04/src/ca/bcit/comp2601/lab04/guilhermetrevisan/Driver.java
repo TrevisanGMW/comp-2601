fe767ab1cb31023fadc021f3e2ac0e3faa68a2fb
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-16 16:46:28 -0700

    Added requested tests

--- lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/Driver.java
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/Driver.java
@@ -1,23 +1,49 @@
 package ca.bcit.comp2601.lab04.guilhermetrevisan;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * See docs
+ * @author Guilherme Trevisan
+ * @version 0.0.1
+ * @since   2022-09-16
  */
 public class Driver {
     public static void main(String[] args) {
-        // create a CompactDisc object and assign the address to a MusicMedia reference
-        CompactDisc cd1;
-        MusicMedia mm1;
-        cd1 = new CompactDisc("TitleOne", "ArtistOne", 7);
-        mm1 = cd1;
+        List<MusicMedia> musicMedias;
+        MusicMedia cd1;
+        MusicMedia cd2;
+        MusicMedia af1;
+        MusicMedia af2;
+        MusicMedia vr1;
+        MusicMedia vr2;
 
-        // display the objects' field values
-        System.out.println(cd1.toString());
-        System.out.println(mm1.toString());
+        musicMedias = new ArrayList();
+        cd1 = new CompactDisc("TitleCdOne", "ArtistCdOne", 5);
+        cd2 = new CompactDisc("TitleCdTwo", "ArtistCdTwo", 10);
+        af1 = new AudioFile("TitleFileOne", "ArtistFileOne", "FileNameOne.mp3", 320);
+        af2 = new AudioFile("TitleFileTwo", "ArtistFileTwo", "FileNameTwo.wav", 256);
+        vr1 = new VinylRecord("TitleVinylOne", "ArtistVinylOne", 3, 10, 100);
+        vr2 = new VinylRecord("TitleVinylTwo", "ArtistVinylTwo");
 
-        // call the play() method.
-        cd1.play();
-        mm1.play();
+        musicMedias.add(cd1);
+        musicMedias.add(cd2);
+        musicMedias.add(af1);
+        musicMedias.add(af2);
+        musicMedias.add(vr1);
+        musicMedias.add(vr2);
+
+        for (MusicMedia musicMediaObject : musicMedias) {
+            System.out.println("Current Object: "  +musicMediaObject.toString()); // toString
+            System.out.println("Reading Method: " + musicMediaObject.getReadingMethod()); // reading method
+            musicMediaObject.play(); // Play
+            if (musicMediaObject instanceof FileManager) {
+                AudioFile audioFile;
+                audioFile = (AudioFile) musicMediaObject;
+                audioFile.save(audioFile.getFileName());
+                audioFile.delete(audioFile.getFileName());
+            }
+            System.out.println("_____________________");
+        }
     }
 }
