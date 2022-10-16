78aa2afb00a7425e2e3f218cb2cd79792a671629
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-15 22:22:57 -0700

    Created class with lab description

---
+++ lab04/lab04/src/ca/bcit/comp2601/lab04/guilhermetrevisan/MusicMedia.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2601.lab04.guilhermetrevisan;
+
+/**
+ * o	An abstract class with fields for song title and artist.
+ * o	Constructor that takes parameters to set both fields. Parameters must be validated appropriately before they are assigned to the fields. If either parameter is invalid throw an IllegalArgumentException and do nothing else (let the program terminate).
+ * o	Getter and setter for each field
+ * o	toString() to display the field values
+ * o	Abstract method void play()
+ */
+public abstract class MusicMedia {
+}
