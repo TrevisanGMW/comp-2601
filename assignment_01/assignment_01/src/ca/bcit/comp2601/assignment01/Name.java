48ee080c5ad76590f86d6425f02624609f9d2dd2
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-17 20:42:25 -0700

    Updated javadocs

--- assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Name.java
+++ assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Name.java
@@ -1,17 +1,7 @@
 package ca.bcit.comp2601.assignment01;
 
 /**
- * Name class
- * This class has final instance variables, constructor arguments,
- * and accessor methods for String first and String last.
- * It has the following methods:
- * public Name(String first, String last): This constructor throws IllegalArgumentException exceptions if either
- *                                         first or last is null or blank.
- * public String getPrettyName(): this method returns a formatted version of the full name; e.g. if first is "tiGEr"
- *                                and last is "woODs", this method returns "Tiger Woods".
- * public String getInitials(): this method returns the initials of the full name;
- *                              e.g. if first is "tiGEr" and last is "woODs", this method returns "T.W.".
- *
+ * Name Class
  * @author Guilherme Trevisan
  * @version 0.0.1
  * @since 2022-10-10
@@ -66,6 +56,7 @@ public class Name {
                 last.substring(FIRST_CHAR_INDEX, SECOND_CHAR_INDEX).toUpperCase() + ".";
     }
 
+    // Getters
     /**
      * Getter First Name
      * @return first
