e65fc984eb853c8745b6c90aabc6e5ffc11cc7d2
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-24 21:03:01 -0700

    Added date tests

--- assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Main.java
+++ assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Main.java
@@ -1,5 +1,8 @@
 package ca.bcit.comp2601.assignment01;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author Guilherme Trevisan
@@ -9,6 +12,24 @@ package ca.bcit.comp2601.assignment01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List dates;
+        dates = new ArrayList<>();
+
+        Date d1;
+        Date d2;
+        Date d3;
+        Date d4;
+        d1 = new Date(31, 12, 2020);
+        d2 = d1.next();
+        d3 = new Date(1, 1, 2021);
+        d4 = d3.previous();
+
+        dates.add(d1);
+        dates.add(d2);
+        dates.add(d3);
+        dates.add(d4);
+        dates.forEach(System.out::println);
+
     }
 }
