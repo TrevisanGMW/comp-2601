abcfdd221c1c793cb24a843d4f25b0b9717e0663
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-24 21:35:16 -0700

    Added compareTo date tests

--- assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Main.java
+++ assignment_01/assignment_01/src/ca/bcit/comp2601/assignment01/Main.java
@@ -1,5 +1,6 @@
 package ca.bcit.comp2601.assignment01;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -21,15 +22,20 @@ public class Main {
         Date d3;
         Date d4;
         d1 = new Date(31, 12, 2020);
-        d2 = d1.next();
+        d2 = d1.next().next().next();
         d3 = new Date(1, 1, 2021);
-        d4 = d3.previous();
+        d4 = d3.previous().previous().previous();
 
         dates.add(d1);
         dates.add(d2);
         dates.add(d3);
         dates.add(d4);
         dates.forEach(System.out::println);
+        System.out.println("______");
+
+        Collections.sort(dates);
+        dates.forEach(System.out::println); // TODO, CHECK IF FOLLOWS HIS DESIRED PATTERN
+
 
     }
 }
