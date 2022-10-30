fb3e88df027a1faaa765a32381ac7bf7d08efc0b
Guilherme Trevisan <trevisangmw@gmail.com>
2022-10-29 18:15:41 -0700

    Created Exception

---
+++ lab06/lab06/src/ca/bcit/comp2601/lab6/guilherme/MissingFileName.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2601.lab6.guilherme;
+
+/**
+ * MissingFileName Exception
+ */
+public class MissingFileName extends Exception {
+    /**
+     * MissingFileName Constructor
+     * @param message message to display as an exception
+     */
+    public MissingFileName(String message) {
+        super(message);
+    }
+}
