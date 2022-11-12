4998f2505db9b33fb4f391e2cfadf1e1c2ccb8ac
Guilherme Trevisan <trevisangmw@gmail.com>
2022-11-12 11:43:01 -0800

    Initial upload

--- quiz_03/quiz_03/src/ca/bcit/comp2601/quiz_03/Main.java
+++ quiz_03/quiz_03/src/ca/bcit/comp2601/quiz_03/Main.java
@@ -1,7 +1,59 @@
 package ca.bcit.comp2601.quiz_03;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Writes commandline arguments to a file, then read this file and prints the result.
+ * @author Guilherme Trevisan
+ * @version 0.0.1
+ * @since 2022-11-12
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String OUTPUT_FILE_NAME;
+
+    static {
+        OUTPUT_FILE_NAME = "data.txt";
+    }
+
+    /**
+     * Stored cmdline arguments to a file then read the file and print content in uppercase.
+     * @param args commandline arguments
+     */
+    public static void main(final String[] args) {
+        FileWriter writer;
+        FileReader reader;
+        Scanner scanner;
+
+        // Save file
+        try {
+            writer = new FileWriter(OUTPUT_FILE_NAME);
+            for (String object : args) {
+                writer.write(object + System.lineSeparator());
+            }
+            writer.close();
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Do nothing
+        }
+
+        // Read file
+        try {
+            reader = new FileReader(OUTPUT_FILE_NAME);
+            scanner = new Scanner(reader);
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line.toUpperCase());
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Do nothing
+        }
     }
 }
