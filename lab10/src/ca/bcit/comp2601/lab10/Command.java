ff5e775993fc9cc614f0fcd6daa9e91a0ffd037c
Guilherme Trevisan <trevisangmw@gmail.com>
2022-11-26 13:39:52 -0800

    Initial upload

---
+++ lab10/src/ca/bcit/comp2601/lab10/Command.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2601.lab10;
+
+/*
+ * Command interface for implementing concrete command
+ */
+public interface Command {
+    /**
+     * Execute method to be later implemented
+     */
+    public void execute();
+}
