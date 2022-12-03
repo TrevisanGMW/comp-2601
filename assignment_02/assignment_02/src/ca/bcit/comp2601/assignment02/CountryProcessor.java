09a8058f59f618f82c6cbdf18868b6c588f7f188
Guilherme Trevisan <trevisangmw@gmail.com>
2022-12-02 21:22:27 -0800

    Finished first pass

--- assignment_02/assignment_02/src/ca/bcit/comp2601/assignment02/CountryProcessor.java
+++ assignment_02/assignment_02/src/ca/bcit/comp2601/assignment02/CountryProcessor.java
@@ -3,6 +3,7 @@ package ca.bcit.comp2601.assignment02;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * CountryProcessor class
@@ -11,18 +12,7 @@ import java.util.*;
  * (e.g. "Canada": "Ottawa"). Use streams and filters to create the following functions which do exactly what they say.
  * In all cases, before printing, collect the result into a java collection local variable:
  *
- * 1. printLongestCapitalCity()
- * 2. printShortestCountryName()
- * 3. printAllCountriesStartingWith(String substring)
- * 4. printLongestCombination() // longest combination of country name plus capital city name
- * 5. printHowManyLettersInCountries() // the total number of letters in all the country names put together
- * 6. printCapitalsWithThisManyLetters(int min, int max) // e.g. all capitals between 5 and 8 letters inclusive
- * 7. printAllCountriesThatDoNotEndWith(char letter)
- * 8. printAllCapitalsThatContainLetterIntoASingleStringNoSpaces(char letter)
- *                  // e.g. containing 'a': "CanadaChadArgentinaNewZealandAustralia…"
- *
- * ProcessCountries() that calls all eight of the following functions.
- * Use the data from the countries-and-capitals.txt file.
+ * ProcessCountries() that calls all eight stream functions.
  *
  * @author Guilherme Trevisan, Monika Szucs
  * @version 0.0.1
@@ -33,6 +23,8 @@ public class CountryProcessor {
     private static final String              DELIMITER;
     private static final int                 OFFSET_COUNTRY;
     private static final int                 OFFSET_LAST_ELEMENT;
+    private static final int                 INDEX_COUNTRY;
+    private static final int                 INDEX_CAPITAL;
     private final Map<String, String>        countriesCapitals;
 
     static {
@@ -40,6 +32,8 @@ public class CountryProcessor {
         DELIMITER           = ",";
         OFFSET_COUNTRY      = -1;
         OFFSET_LAST_ELEMENT = -1;
+        INDEX_COUNTRY       = 0;
+        INDEX_CAPITAL       = 1;
     }
 
     /**
@@ -47,24 +41,17 @@ public class CountryProcessor {
      * @throws FileNotFoundException if necessary input file is missing
      */
     public CountryProcessor() throws FileNotFoundException {
-
-        countriesCapitals = new HashMap<String, String>();
-
+        countriesCapitals = new HashMap<>();
         // Populate countriesCapitals with clean data
         Scanner file = new Scanner(new File(INPUT_FILE));
         while(file.hasNextLine()) {
             String line = file.nextLine();
-            String countryCapitalArray[] = parseCountryCapitalLineAsArray(line);
+            String[] countryCapitalArray = parseCountryCapitalLineAsArray(line);
             if (countryCapitalArray != null){
-                countriesCapitals.put(countryCapitalArray[0], countryCapitalArray[1]);
+                countriesCapitals.put(countryCapitalArray[INDEX_COUNTRY], countryCapitalArray[INDEX_CAPITAL]);
             }
         }
         file.close();
-
-
-        for (Object entry : countriesCapitals.entrySet()) {
-            System.out.println(entry);
-        }
     }
 
     /**
@@ -72,14 +59,13 @@ public class CountryProcessor {
      * Assumes that the pattern is country, capital
      * Since some countries can have commas, the parsing method works its way from the last element (capital -> country)
      * @param commaSeparatedLine a line containing country, capital
-     *                           e.g. Congo, Republic of the,Brazzaville
+     *                           e.g. "Congo, Republic of the,Brazzaville"
      * @return a string array where the first element (0) is the country and the second (1) is the capital
      */
     private static String[] parseCountryCapitalLineAsArray(final String commaSeparatedLine){
         try{
             String country;
             String capital;
-            int yearPublished;
             List<String> countryCapitalData;
             countryCapitalData = new ArrayList<>(List.of(commaSeparatedLine.split(DELIMITER)));
             capital = countryCapitalData.get(countryCapitalData.size()+OFFSET_COUNTRY);
@@ -87,19 +73,129 @@ public class CountryProcessor {
             country = String.join(DELIMITER, countryCapitalData);
             return new String[]{country, capital};
         } catch(Exception e) {
+            e.printStackTrace();
             return null;
         } finally {
             // Do nothing
         }
     }
 
+    /**
+     * Print the longest capital (character length)
+     * Uses "stream" to accomplish that
+     */
     public void printLongestCapitalCity(){
-//        List<String> authorsFiltered = novels.stream()
-//                .filter(n->n.getYearPublished() >= firstYear && n.getYearPublished() <= lastYear)
-//                .sorted(Comparator.comparing(Novel::getYearPublished))
-//                .map(n -> n.getAuthorName())
-//                .collect(Collectors.toList());
-//        return authorsFiltered;
+        Optional<String> longest = countriesCapitals.values().stream()
+                .filter(s -> !s.isBlank())
+                .max(Comparator.comparing(String::length));
+
+        longest.ifPresent(s-> System.out.println("Longest capital city is \"" + s + "\"."));
+    }
+
+    /**
+     * Print the shortest country (character length)
+     * Uses "stream" to accomplish that
+     */
+    public void printShortestCountryName(){
+        Optional<String> shortest = countriesCapitals.entrySet().stream()
+                .filter(m -> !m.getKey().isBlank() && m.getKey() != null)
+                .map(Map.Entry::getKey)
+                .min(Comparator.comparing(String::length));
+
+        shortest.ifPresent(s-> System.out.println("Shortest country name is \"" + s + "\"."));
+    }
+
+    /**
+     * Print all countries starting with provided substring
+     * @param substring substring that country starts with
+     */
+    public void printAllCountriesStartingWith(final String substring){
+        List containingString = countriesCapitals.entrySet().stream()
+                .filter(m->!m.getKey().isBlank() && m.getKey() != null && m.getKey().startsWith(substring))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println("Printing all countries starting with \"" + substring + "\".");
+        containingString.forEach(System.out::println);
+    }
+
+    /**
+     * Print longest combination of country name plus capital city name
+     */
+    public void printLongestCombination(){
+        Optional<String> longestCombination = countriesCapitals.entrySet().stream()
+                .filter(m -> !m.getKey().isBlank() && m.getKey() != null &&
+                        !m.getValue().isBlank() && m.getValue() != null)
+                .map(m-> m.getKey() + DELIMITER + " " + m.getValue())
+                .max(Comparator.comparing(String::length));
+
+        longestCombination.ifPresent(s-> System.out.println("Longest combination (capital+country) name is \"" + s + "\"."));
+    }
+
+    /**
+     * Print the total number of letters in all the country names put together
+     */
+    public void printHowManyLettersInCountries(){
+        String result = countriesCapitals.entrySet().stream()
+                .filter(m -> !m.getKey().isBlank() && m.getKey() != null)
+                .map(map -> map.getValue())
+                .collect(Collectors.joining());
+        System.out.println("Number of letters in all the country names put together: " + result.length());
+    }
+
+    /**
+     * Print all capitals between 5 and 8 letters inclusive
+     * @param min minimum number of characters to be included
+     * @param max maximum number of characters to be included
+     */
+    public void printCapitalsWithThisManyLetters(final int min, final int max){
+        List matchingLength = countriesCapitals.values().stream()
+                .filter(s->!s.isBlank() && s != null && s.length() >= min && s.length() <= max)
+                .collect(Collectors.toList());
+
+        System.out.println("Printing all capitals with within character range (min:" + min + ", max:" + max + ")");
+        matchingLength.forEach(System.out::println);
+    }
+
+    /**
+     * Print all countries that do not end with provided letter
+     * @param letter letter to filter
+     */
+    public void printAllCountriesThatDoNotEndWith(final char letter){
+        List containingString = countriesCapitals.entrySet().stream()
+                .filter(m->!m.getKey().isBlank() && m.getKey() != null && !m.getKey().endsWith(String.valueOf(letter)))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println("Printing all countries that do not end with \"" + letter + "\".");
+        containingString.forEach(System.out::println);
+    }
+
+    /**
+     * Print all countries that contain letter into a single string
+     * e.g. containing 'a': "CanadaChadArgentinaNewZealandAustralia…"
+     * (Even though this example shows countries, I'm assuming these were supposed to be capitals)
+     * @param letter substring to find (should contain)
+     */
+    public void printAllCapitalsThatContainLetterIntoASingleStringNoSpaces(char letter){
+        String containingString = countriesCapitals.values().stream()
+                .filter(s->!s.isBlank() && s != null && s.contains(String.valueOf(letter)))
+                .map(s->s.replaceAll(" ", ""))
+                .collect(Collectors.joining());
+        System.out.println("Printing all capitals that contain the letter \"" + letter + "\" combined.");
+        System.out.println(containingString);
+    }
+
+    /**
+     * Calls all eight stream and filters functions
+     */
+    public void processCountries(){
+        printLongestCapitalCity();
+        printShortestCountryName();
+        printAllCountriesStartingWith("Co");
+        printLongestCombination();
+        printHowManyLettersInCountries();
+        printCapitalsWithThisManyLetters(4, 6);
+        printAllCountriesThatDoNotEndWith('a');
+        printAllCapitalsThatContainLetterIntoASingleStringNoSpaces('y');
     }
 
 }
