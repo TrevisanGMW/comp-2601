3faf2287a6bfa3d690b475ed8be97cf235512b59
Guilherme Trevisan <trevisangmw@gmail.com>
2022-12-03 11:55:56 -0800

    Implemented adapter

---
+++ assignment_02/assignment_02/src/ca/bcit/comp2601/assignment02/patterns/Movable.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2601.assignment02.patterns;
+
+/**
+ * MovableAdapter interface to get speed in MPH
+ *
+ * @author Guilherme Trevisan, Monika Szucs
+ * @version 0.0.1
+ * @since 2022-12-03
+ */
+public interface Movable {
+    /**
+     * // returns speed in MPH
+     * @return speed in MPH
+     */
+    double getSpeed();
+}
