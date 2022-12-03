ee9abe885c5d1cec6e8967f7d5b7fb7781f109ad
Guilherme Trevisan <trevisangmw@gmail.com>
2022-12-03 12:23:44 -0800

    Add javadocs

--- assignment_02/assignment_02/src/ca/bcit/comp2601/assignment02/patterns/BugattiVeyron.java
+++ assignment_02/assignment_02/src/ca/bcit/comp2601/assignment02/patterns/BugattiVeyron.java
@@ -2,6 +2,7 @@ package ca.bcit.comp2601.assignment02.patterns;
 
 /**
  * BugattiVeyron class
+ * Ideally it would extend a luxury car base class, but was kept like this for simplicity.
  *
  * @author Guilherme Trevisan, Monika Szucs
  * @version 0.0.1
@@ -11,12 +12,20 @@ public class BugattiVeyron implements Movable {
 
     private final double speed;
 
+    /**
+     * BugattiVeyron constructor
+     * @param speed car's speed
+     */
     public BugattiVeyron(double speed) {
         this.speed = speed;
     }
 
+    /**
+     * Movable implemetantion
+     * @return speed
+     */
     @Override
     public double getSpeed() {
-        return 268;
+        return speed;
     }
 }
